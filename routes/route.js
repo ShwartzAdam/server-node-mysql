629041e1bb7ce1746ee250588dec7825f347652b
ShwartzAdam <adam@shwartz.co>
2018-02-12 16:14:51 +0200

    Add files via upload

---
+++ routes/route.js
@@ -0,0 +1,21 @@
+//custom route for fetching data
+var transactions = require('../data_access/transaction');
+
+module.exports = {
+    //set up route configuration that will be handle by express server
+    configure: function (app) {
+
+        // adding route for users, here app is express instance which provide use
+        // get method for handling get request from http server.
+        app.get('/api/users', function (req, res) {
+            transactions.getAllUsers(res);
+        });
+
+        // here we gets id from request and passing to it transaction method.
+        app.get('/api/transactions/:id/', function (req, res) {
+            transactions.getTransactionById(req.params.id, res);
+        });
+
+    }
+
+};
